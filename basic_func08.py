9b8e07594dbf8338f0c43f2ad8e8c1eea968bf08
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-22 22:17:30 +0500

    7,8,9

--- basic_func08.py
+++ basic_func08.py
@@ -1,5 +1,6 @@
 # Create a function called main.
-
+def main():
+    a=0
 # Create function arguments a.
-
+    return a+1
 # Increase the value of a to one and return.
--- basic_func10.py
+++ basic_func10.py
@@ -1,5 +1,6 @@
 # Create a function called main.
-
+def main():
+    a=7
 # Create function arguments a.
-
+    return -a
 # Return the negative value of a.
