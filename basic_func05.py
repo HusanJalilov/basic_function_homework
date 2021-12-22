db3c307bb810c11db94cc669ca8b23599d199419
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2021-12-22 22:12:31 +0500

    5,6

--- basic_func05.py
+++ basic_func05.py
@@ -1,3 +1,4 @@
 # Create a function called main.
-
+def main():
+    return 2,1
 # Return the value float type.
--- basic_func07.py
+++ basic_func07.py
@@ -1,5 +1,6 @@
 # Create a function called main.
-
+def main():
+    a=21
 # Create function arguments a.
-
+    return a
 # Return the value a.
